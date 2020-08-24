a8052427c6827e33eb77cb1dd5a08c8b18d44403
nekomaho <nekosatoru@gmail.com>
2020-08-24 21:57:17 +0900

    Change fetch_value allow multi config types

--- src/config.rs
+++ src/config.rs
@@ -9,27 +9,23 @@ pub struct Config {
 }
 
 trait FetchValue {
-    fn value(
-        config_yaml: &yaml_rust::yaml::Yaml,
-        element: &str,
-    ) -> Result<String, i32> {
-        match config_yaml[element].as_str() {
-            Some(value) => Ok(value.to_string()),
-            None => {
-                println!("{} is empty", element);
-                Err(1)
-            }
-        }
-    }
     fn fetch_value(
         config_yaml: &yaml_rust::yaml::Yaml,
-        config_type: &str,
-        element: &str,
+        config_types: &Vec<&str>
     ) -> Result<String, i32> {
-        match config_yaml[config_type][element].as_str() {
+        let mut config = config_yaml;
+        for config_type in config_types {
+          config =  &config[*config_type];
+        }
+
+        match config.as_str() {
             Some(value) => Ok(value.to_string()),
             None => {
-                println!("{} is empty", element);
+                let mut output = String::new(); 
+                for config_type in config_types {
+                    output = format!("{} {}", config_type, output);
+                }
+                println!("{} is empty", output);
                 Err(1)
             }
         }
@@ -117,10 +113,10 @@ impl OauthConfig {
 
         for config_element in oauth_vec {
             let oauth = OauthConfig::new(
-                &OauthConfig::value(&config_element, "name")?,
-                &OauthConfig::value(&config_element, "client_id")?,
-                &OauthConfig::value(&config_element, "client_secret")?,
-                &OauthConfig::value(&config_element, "token_url")?,
+                &OauthConfig::fetch_value(&config_element, &vec!["name"])?,
+                &OauthConfig::fetch_value(&config_element, &vec!["client_id"])?,
+                &OauthConfig::fetch_value(&config_element, &vec!["client_secret"])?,
+                &OauthConfig::fetch_value(&config_element, &vec!["token_url"])?,
             );
             oauth_settings.push(oauth);
         }
@@ -141,7 +137,7 @@ impl OauthConfig {
 impl TokenConfig {
     pub fn new(config: &yaml_rust::yaml::Yaml) -> Result<Self, i32> {
         Ok(TokenConfig {
-            scope: TokenConfig::fetch_value(config, "token", "scope")?,
+            scope: TokenConfig::fetch_value(config, &vec!["token", "scope"])?,
         })
     }
 }
@@ -149,9 +145,9 @@ impl TokenConfig {
 impl ApiConfig {
     pub fn new(config: &yaml_rust::yaml::Yaml) -> Result<Self, i32> {
         Ok(ApiConfig {
-            method: ApiConfig::fetch_value(config, "api", "method")?,
-            url: ApiConfig::fetch_value(config, "api", "url")?,
-            content_type: ApiConfig::fetch_value(config, "api", "content_type")?,
+            method: ApiConfig::fetch_value(config, &vec!["api", "method"])?,
+            url: ApiConfig::fetch_value(config, &vec!["api", "url"])?,
+            content_type: ApiConfig::fetch_value(config, &vec!["api", "content_type"])?,
             body: ApiConfig::fetch_value_allow_empty(config, "api", "body"),
         })
     }
